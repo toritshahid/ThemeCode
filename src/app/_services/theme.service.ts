0c63765b5c455198172315909adc10a930e469cc
toritshahid <torit.shahid@gmail.com>
2021-09-02 18:21:15 +0530

    fixed

--- src/app/_services/theme.service.ts
+++ src/app/_services/theme.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Inject, EventEmitter } from '@angular/core';
+import { darkTheme } from '../themes/dark-theme';
 import { THEMES, ACTIVE_THEME, Theme } from '../themes/symbols';
 
 @Injectable()
@@ -6,23 +7,25 @@ export class ThemeService {
 
   themeChange = new EventEmitter<Theme>();
 
-  constructor(
-    @Inject(THEMES) public themes: Theme[],
-    @Inject(ACTIVE_THEME) public theme: string
-  ) {
+  constructor(@Inject(THEMES) public themes: Theme[], @Inject(ACTIVE_THEME) public theme: string) {
+    const darkModeOn =window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+      if(darkModeOn){
+        this.setTheme('dark');
+      }
   }
 
+
   getActiveTheme() {
-    const theme = this.themes.find(t => t.name === this.theme);
-    if (!theme) {
-      throw new Error(`Theme not found: '${this.theme}'`);
-    }
-    return theme;
+      const theme = this.themes.find(t => t.name === this.theme);
+      if (!theme) {
+        throw new Error(`Theme not found: '${this.theme}'`);
+      }
+      return theme;
   }
 
   setTheme(name: string) {
     this.theme = name;
-    this.themeChange.emit( this.getActiveTheme());
+    this.themeChange.emit(this.getActiveTheme());
   }
 
 }
